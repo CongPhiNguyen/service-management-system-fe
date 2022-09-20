c8954b7241a62bef7dcd7291d90a54bb87411395
CongPhiNguyen <congphinguyen312@gmail.com>
2022-09-20 09:35:28 +0700

    merge fix

---
+++ src/features/EditService/components/AddJson.js
@@ -0,0 +1,285 @@
+import React, { useEffect, useState } from "react";
+
+import { JsonEditor as Editor } from "jsoneditor-react";
+import "jsoneditor-react/es/editor.min.css";
+import sampleData from "./sampledata";
+import { Button, Segmented, Modal, Typography, message } from "antd";
+
+import ace from "brace";
+import "brace/mode/json";
+import "brace/theme/github";
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/theme-monokai";
+import { FastBackwardFilled } from "@ant-design/icons";
+
+export default function AddJson() {
+  // console.log(sampleData);
+  const [currentSegment, setCurrentSegment] = useState("json_ide");
+  const [disableConvert, setDisableConvert] = useState(true);
+  const [globalJSONValue, setGlobalJSONValue] = useState("");
+  const [globalObjectValue, setGlobalObjectValue] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isValidInputJSON, setIsValidInputJSON] = useState(false);
+
+  const checkJSONValid = (text) => {
+    if (text.length === 0) return false;
+    try {
+      JSON.parse(text);
+    } catch (e) {
+      return false;
+    }
+    return true;
+  };
+
+  const makeDefaultTemplate = () => {
+    setGlobalJSONValue(sampleData);
+  };
+
+  const checkIsValidServices = (value) => {
+    const val = JSON.parse(value);
+    if (!val.serviceName || val.serviceName.length === 0) {
+      return {
+        valid: false,
+        message: "The serviceName is not found or blank",
+      };
+    }
+    if (!val.author || val.author.length === 0) {
+      return {
+        valid: false,
+        message: "The author email is not found or blank",
+      };
+    }
+    if (!val.authorizedPerson || val.authorizedPerson.length === 0) {
+      return {
+        valid: false,
+        message: "The authorized person email is not found or blank",
+      };
+    }
+    const regexTestEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (!regexTestEmail.test(val.author)) {
+      return {
+        valid: false,
+        message: "Author email format not valid",
+      };
+    }
+    if (!regexTestEmail.test(val.authorizedPerson)) {
+      return {
+        valid: false,
+        message: "Authorized email format not valid",
+      };
+    }
+    if (val.isPublic == undefined) {
+      return {
+        valid: false,
+        message: "Service status is null",
+      };
+    }
+
+    const checkBlankWithZeroLength = (val, valueString) => {
+      if (val === undefined || val === null)
+        return {
+          valid: false,
+          message: `${valueString} is undefined`,
+        };
+      else
+        return {
+          valid: true,
+        };
+    };
+
+    const checkBlank = (val, valueString) => {
+      if (val === undefined || val === null || val.length === 0)
+        return {
+          valid: false,
+          message: `${valueString} is null or undefined`,
+        };
+      else
+        return {
+          valid: true,
+        };
+    };
+
+    let checkStep = checkBlank(val.version, "Version");
+    if (!checkStep.valid) return checkStep;
+
+    checkStep = checkBlank(
+      val.monitoring?.endpointPublicUrl,
+      "Monitoring endpointPublicUrl"
+    );
+    if (!checkStep.valid) return checkStep;
+
+    checkStep = checkBlank(
+      val.monitoring?.endpointPrivateUrl,
+      "Monitoring endpointPrivateUrl"
+    );
+    if (!checkStep.valid) return checkStep;
+
+    checkStep = checkBlankWithZeroLength(
+      val.monitoring?.alertTo,
+      "Monitoring alert"
+    );
+    if (!checkStep.valid) return checkStep;
+
+    checkStep = checkBlank(val.monitoring?.alertBot, "Monitoring alert bot");
+    if (!checkStep.valid) return checkStep;
+
+    checkStep = checkBlank(val.requirement?.domain, "Service domain");
+    if (!checkStep.valid) return checkStep;
+
+    checkStep = checkBlank(val.requirement?.port, "Service port");
+    if (!checkStep.valid) return checkStep;
+
+    checkStep = checkBlank(val.requirement?.platform, "Service platform");
+    if (!checkStep.valid) return checkStep;
+
+    checkStep = checkBlank(
+      val.requirement?.serviceDependencies,
+      "Service serviceDependencies"
+    );
+    if (!checkStep.valid) return checkStep;
+
+    checkStep = checkBlankWithZeroLength(
+      val.requirement?.ownDependencies,
+      "Service ownDependencies"
+    );
+    if (!checkStep.valid) return checkStep;
+
+    checkStep = checkBlankWithZeroLength(
+      val.requirement?.serviceDependencies,
+      "Service Dependencies"
+    );
+    if (!checkStep.valid) return checkStep;
+
+    checkStep = checkBlank(
+      val.requirement?.infrastructure,
+      "Service Infrastructure"
+    );
+    if (!checkStep.valid) return checkStep;
+
+    checkStep = checkBlank(val.requirement?.database, "Service database");
+    if (!checkStep.valid) return checkStep;
+
+    return {
+      valid: true,
+    };
+  };
+
+  const addService = () => {
+    setIsModalOpen(true);
+  };
+
+  const addServiceHandle = () => {
+    if (!checkJSONValid(globalJSONValue))
+      message.error("File json định dạng lỗi");
+
+    const resCheckValid = checkIsValidServices(globalJSONValue);
+
+    if (resCheckValid.valid) {
+      message.success("Thêm json thành công");
+    } else {
+      message.error(resCheckValid.message);
+    }
+    // console.log(JSON.parse(globalJSONValue));
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div>
+      <Segmented
+        options={[
+          // {
+          //   label: <div>Upload</div>,
+          //   value: "upload_file",
+          // },
+          {
+            label: <div>JSON IDE</div>,
+            value: "json_ide",
+            disabled: currentSegment !== "json_ide" && disableConvert,
+          },
+          {
+            label: <div>JSON Editor</div>,
+            value: "json_editor",
+            disabled: currentSegment !== "json_editor" && disableConvert,
+          },
+        ]}
+        onChange={(value) => {
+          console.log(value);
+          setCurrentSegment(value);
+          setGlobalJSONValue((prev) => {
+            return JSON.stringify(JSON.parse(prev), null, "\t");
+          });
+        }}
+      />
+      {currentSegment === "json_editor" ? (
+        <div>
+          <Editor
+            value={JSON.parse(globalJSONValue)}
+            onChange={(value) => {
+              try {
+                JSON.stringify(value);
+                setGlobalJSONValue(JSON.stringify(value));
+                if (checkJSONValid(JSON.stringify(value)))
+                  setDisableConvert(false);
+                else setDisableConvert(true);
+              } catch (err) {
+                setDisableConvert(true);
+              }
+            }}
+          />
+          <div className="button-container mt-2 flex ">
+            <Button onClick={makeDefaultTemplate}>Use default</Button>
+            <Button
+              type="primary"
+              onClick={addService}
+              disabled={!isValidInputJSON}
+            >
+              Add service
+            </Button>
+          </div>
+        </div>
+      ) : currentSegment === "json_ide" ? (
+        <div>
+          <AceEditor
+            className="!w-[800px] !h-[300px]"
+            value={globalJSONValue}
+            mode="json"
+            theme="monokai"
+            onChange={(value) => {
+              setIsValidInputJSON(checkJSONValid(value));
+              setGlobalJSONValue(value);
+              if (checkJSONValid(value)) setDisableConvert(false);
+              else setDisableConvert(true);
+            }}
+            name="UNIQUE_ID_OF_DIV"
+            fontSize={14}
+            highlightActiveLine={true}
+            editorProps={{ $blockScrolling: true }}
+          />
+          <div className="button-container mt-2 flex ">
+            <Button onClick={makeDefaultTemplate}>Use default</Button>
+            <Button
+              type="primary"
+              onClick={addService}
+              disabled={!isValidInputJSON}
+            >
+              Add service
+            </Button>
+          </div>
+        </div>
+      ) : null}
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onOk={addServiceHandle}
+        onCancel={handleCancel}
+        className="w-[400px]"
+      >
+        <Typography.Text>Bạn có chắc chắn muốn thêm service?</Typography.Text>
+      </Modal>
+    </div>
+  );
+}
--- src/features/EditService/pages/EditServicesPage.js
+++ src/features/EditService/pages/EditServicesPage.js
@@ -1,38 +1,35 @@
 import { Tabs, Layout, Typography } from "antd";
 import React, { useState, useEffect } from "react";
-import AddJson from "../../research/AddServices";
+import AddJson from "../components/AddJson.js";
 import EditServiceByForm from "../components/EditServiceByForm";
-import { get } from "../../../api/axios"
-import URL from "../../../api/config"
+import { get } from "../../../api/axios";
+import URL from "../../../api/config";
 
 /* eslint-disable */
 const { Content } = Layout;
 const { Title } = Typography;
 
 export default function EditServicesPage() {
-
-
-
-    return (
-        <div>
-            <Content
-                className="mt-8"
-                style={{
-                    padding: "0 200px",
-                }}
-            >
-                <Title className="text-center" level={2}>
-                    Edit Service
-                </Title>
-                <Tabs size="large" defaultActiveKey="1">
-                    <Tabs.TabPane tab="Form" key="12323">
-                        <EditServiceByForm ></EditServiceByForm>
-                    </Tabs.TabPane>
-                    <Tabs.TabPane tab="JSON" key="232121231">
-                        <AddJson />
-                    </Tabs.TabPane>
-                </Tabs>
-            </Content>
-        </div>
-    );
+  return (
+    <div>
+      <Content
+        className="mt-8"
+        style={{
+          padding: "0 200px",
+        }}
+      >
+        <Title className="text-center" level={2}>
+          Edit Service
+        </Title>
+        <Tabs size="large" defaultActiveKey="1">
+          <Tabs.TabPane tab="Form" key="12323">
+            <EditServiceByForm></EditServiceByForm>
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="JSON" key="232121231">
+            <AddJson />
+          </Tabs.TabPane>
+        </Tabs>
+      </Content>
+    </div>
+  );
 }
