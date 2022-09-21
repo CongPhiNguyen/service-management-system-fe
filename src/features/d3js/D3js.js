14d0d77c33b12eb7fe169f09c1e088feda7c608a
Luong Thien Phuoc <phuoc.t.luong@taptap.com.vn>
2022-09-21 13:57:51 +0700

    new

--- src/features/d3js/D3js.js
+++ src/features/d3js/D3js.js
@@ -1,56 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import Tree from 'react-d3-tree';
-import "./custom-tree.css"
-import { useState } from 'react';
-import { v4 } from "uuid";
+import axios from 'axios';
+import './custom-tree.css';
+import { useCenteredTree } from "./helper";
+// This is a simplified example of an org chart with a depth of 2.
+// Note how deeper levels are defined recursively via the `children` property.
 const orgChart = {
     name: 'CEO',
-    attributes: {
-        id: "CEO"
-    },
     children: [
         {
             name: 'Manager',
             attributes: {
                 department: 'Production',
-                id: "123"
             },
             children: [
                 {
                     name: 'Foreman',
                     attributes: {
                         department: 'Fabrication',
-                        id: "567"
                     },
                     children: [
                         {
-                            attributes: {
-                                department: 'Fabrication',
-                                id: "gggg"
-                            },
                             name: 'Worker',
-                        },
-                        {
-                            attributes: {
-                                department: 'Fabrication',
-                                id: "fsdas"
-                            },
-                            name: 'Worker',
-                            children: [
-                                {
-                                    attributes: {
-                                        department: 'Fabrication',
-                                        id: "423"
-                                    },
-                                    name: 'Worker',
-                                },
-                                {
-                                    attributes: {
-                                        id: "kk"
-                                    },
-                                    name: 'Worker',
-                                },
-                            ]
                         },
                     ],
                 },
@@ -58,14 +29,9 @@ const orgChart = {
                     name: 'Foreman',
                     attributes: {
                         department: 'Assembly',
-                        id: "562227"
                     },
                     children: [
                         {
-                            attributes: {
-                                department: 'Assembly',
-                                id: "hẻgwerg"
-                            },
                             name: 'Worker',
                         },
                     ],
@@ -74,28 +40,53 @@ const orgChart = {
         },
     ],
 };
+const containerStyles = {
+    width: "100vw",
+    height: "100vh"
+};
 
-export default function D3js() {
-    const [tree, setTree] = useState(orgChart)
+const straightPathFunc = (linkDatum, orientation) => {
+    const { source, target } = linkDatum;
+    return orientation === 'horizontal'
+        ? `M${source.y},${source.x}L${target.y},${target.x}`
+        : `M${source.x},${source.y}L${target.x},${target.y}`;
+};
 
-    const handleNodeClick = data => {
-        console.log(data);
-
-    }
+export default function OrgChartTree() {
+    const [tree, setTree] = useState(null)
+    const [dimensions, translate, containerRef] = useCenteredTree();
+    useEffect(() => {
+        axios.get("http://localhost:5050/api/v1/service/get-tree/632a93de2924f07fc8924697")
+            .then(res => {
+                console.log(res.data)
+                setTree(res.data.tree)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }, [])
 
     return (
-        <div id="treeWrapper" className='mt-[50px] ml-[50px]' style={{ width: '100em', height: '50em', border: '1px solid black' }}>
+        // `<Tree />` will fill width/height of its container; in this case `#treeWrapper`.
+        <div id="treeWrapper" ref={containerRef} style={{ width: '100vw', height: '100vh', background: "linear-gradient(0deg,#6a82fb,#fc5c7d)" }}>
             <Tree
-                data={tree}
+                data={tree ? tree : orgChart}
+                orientation={"vertical"}
+                translate={{ x: 700, y: 100 }}
+                transitionDuration={500}
+                enableLegacyTransitions={true}
+                zoomable={true}
+                // CSS
                 rootNodeClassName="node__root"
                 branchNodeClassName="node__branch"
                 leafNodeClassName="node__leaf"
-                orientation='vertical'
-                zoomable={true}
-                zoom={0.5}
-                translate={{ x: 300, y: 100 }}
-                onNodeClick={handleNodeClick}
+                dimensions={dimensions}
+                separation={{
+                    nonSiblings: 2,
+                    siblings: 2
+                }}
+            // pathFunc={straightPathFunc}
             />
         </div>
-    )
+    );
 }
