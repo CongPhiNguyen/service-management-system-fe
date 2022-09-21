09aeef17e5cdbd7514adbe6868c69756326b7d5b
CongPhiNguyen <congphinguyen312@gmail.com>
2022-09-21 13:58:38 +0700

    display node better visual

--- src/features/display/NodeDisplay.js
+++ src/features/display/NodeDisplay.js
@@ -3,43 +3,106 @@ import { Stage, Layer, Rect, Text, Circle, Line, Arrow } from "react-konva";
 import { getFix } from "../../api/axios";
 import URL from "../../api/config";
 
+const SCREEN_WIDTH = 1000;
+const SCREEN_HEIGHT = 600;
+
 export default function NodeDisplay(props) {
-  console.log("props", props);
+  // console.log("props", props);
 
   const [currentServiceList, setCurrentServiceList] = useState([]);
   const [serviceNodes, setServiceNodes] = useState([]);
   const [serviceRelations, setServiceRelations] = useState([]);
 
-  const createServiceNodes = (services) => {
-    // const serviceNodesTemp = services.map((val, index) => {
-    //   return {
-    //     index: index,
-    //     _id: val._id,
-    //     name: val.serviceName,
-    //     x: ((index * 1000) % 70) * 10 + 20,
-    //     y: ((index * 1000) % 30) * 10 + 20,
-    //     color: randomColor(),
-    //     dependFor: val.requirement.serviceDependencies,
-    //   };
-    // });
+  const createServiceNodesTree = (depen, current) => {
+    console.log("depen", depen);
+    console.log("current", current);
+
+    let serviceNodesTemp = [];
+
+    serviceNodesTemp.push({
+      index: 0,
+      _id: current._id,
+      name: current.name,
+      x: SCREEN_WIDTH / 2,
+      y: 100 / 2,
+      initX: SCREEN_WIDTH / 2,
+      initY: 100 / 2,
+      color: randomColor(),
+      dependFor: depen.map((val) => val._id),
+    });
+
+    const makeDepenNode = (node, beginX, endX, depth) => {
+      let depenNodeRender = [];
+      let currentDepen = node.dependences;
+      console.log("currentDepen", currentDepen);
+      for (let i = 0; i < currentDepen.length; i++) {
+        depenNodeRender.push(
+          ...makeDepenNode(
+            currentDepen[i],
+            beginX + (i * (endX - beginX)) / currentDepen.length,
+            beginX + ((i + 1) * (endX - beginX)) / currentDepen.length,
+            depth + 1
+          )
+        );
+      }
+      // console.log("depenNodeRender", depenNodeRender);
+      return [
+        {
+          index: 0,
+          _id: node._id,
+          name: node.name,
+          x: beginX + (endX - beginX) / 2,
+          y: depth * 100 + 50,
+          initX: beginX + (endX - beginX) / 2,
+          initY: depth * 100 + 50,
+          color: randomColor(),
+          dependFor: node.dependences.map((val) => val._id),
+        },
+        ...depenNodeRender,
+      ];
+    };
+
+    for (let i = 0; i < depen.length; i++) {
+      console.log("depen", i, depen[i]);
+      serviceNodesTemp.push(
+        ...makeDepenNode(
+          depen[i],
+          0 + (i * (SCREEN_WIDTH - 0)) / depen.length,
+          0 + ((i + 1) * (SCREEN_WIDTH - 0)) / depen.length,
+          1
+        )
+      );
+    }
+
     // console.log("serviceNodesTemp", serviceNodesTemp);
-    // setServiceNodes(serviceNodesTemp);
-    // makeServiceRelation(serviceNodesTemp);
+    let setID = [];
+    serviceNodesTemp = serviceNodesTemp.filter((val) => {
+      if (setID.includes(val._id)) {
+        return false;
+      } else {
+        setID.push(val._id);
+        return true;
+      }
+    });
+    makeServiceRelation(serviceNodesTemp);
+    setServiceNodes(serviceNodesTemp);
   };
 
   const makeServiceRelation = (serviceNodes) => {
+    console.log("serviceNodes", serviceNodes);
     const serviceRelationTemp = [];
     // console.log("serviceNodes abc", serviceNodes);
     const objectFind = {};
     for (let i = 0; i < serviceNodes.length; i++) {
-      objectFind[serviceNodes[i]._id] = serviceNodes[i];
+      if (!objectFind[serviceNodes[i]._id])
+        objectFind[serviceNodes[i]._id] = serviceNodes[i];
     }
-    // console.log("objectFind", objectFind);
+    console.log("objectFind", objectFind);
 
     for (let i = 0; i < serviceNodes.length; i++) {
       const dependFor = serviceNodes[i].dependFor;
       for (let j = 0; j < dependFor.length; j++) {
-        // console.log(objectFind[serviceNodes[i]._id], objectFind[dependFor[j]]);
+        console.log("dependFor[i]", dependFor[i]);
         serviceRelationTemp.push({
           pointA: objectFind[serviceNodes[i]._id],
           pointB: objectFind[dependFor[j]],
@@ -55,17 +118,20 @@ export default function NodeDisplay(props) {
   }
 
   useEffect(() => {
+    setServiceNodes([]);
     const getServiceTree = () => {
       getFix(URL.URL_GET_SERVICE_TREE, { id: props.nodeID })
         .then((data) => {
-          console.log("data", data);
+          if (data.data.success) {
+            createServiceNodesTree(data.data.depen, data.data.current);
+          }
         })
         .catch((err) => {
           console.log("err", err);
         });
     };
     getServiceTree();
-  }, []);
+  }, [props.nodeID]);
 
   // console.log("serviceNodes", serviceNodes);
 
@@ -88,16 +154,19 @@ export default function NodeDisplay(props) {
     ];
   };
 
+  console.log("serviceNodes", serviceNodes);
+
   return (
     <div>
       <Stage width={1900} height={1600} className="w-[100%] h-[100%]">
         <Layer>
           {serviceNodes.map((val, index) => {
+            // console.log("val", val);
             return (
               <React.Fragment>
                 <Circle
-                  x={((index * 1000) % 70) * 10 + 20}
-                  y={((index * 1000) % 30) * 10 + 20}
+                  x={val.initX}
+                  y={val.initY}
                   fill={val.color}
                   radius={20}
                   draggable={true}
@@ -106,12 +175,12 @@ export default function NodeDisplay(props) {
                   }}
                   onDragMove={(e) => {
                     setServiceNodes((prev) => {
-                      return prev.map((val) => {
-                        if (val.index === index) {
-                          val.x = e.target.x();
-                          val.y = e.target.y();
+                      return prev.map((currentVal) => {
+                        if (currentVal._id === val._id) {
+                          currentVal.x = e.target.x();
+                          currentVal.y = e.target.y();
                         }
-                        return val;
+                        return currentVal;
                       });
                     });
                   }}
